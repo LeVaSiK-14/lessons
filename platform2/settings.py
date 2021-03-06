cdb32813395e91a81059b15e64df3cdebf380314
leva-14 <lev201611@gmail.com>
2021-03-06 13:59:45 +0600

    sec com

--- platform2/settings.py
+++ platform2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'a2#$ko-+hn*w$m64z90gzeofw99jv4bw3_+5s@l^$ya&74@t%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
